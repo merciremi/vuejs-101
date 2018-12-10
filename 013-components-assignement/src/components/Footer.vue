7a392fbb961c61f160251a6144bd9d3aa7a78dc9
Remi Mercier <remi.merci@gmail.com>
2018-12-10 14:52:09 +0100

    Add info in app footer

--- 013-components-assignement/src/components/Footer.vue
+++ 013-components-assignement/src/components/Footer.vue
@@ -3,6 +3,8 @@
     <div class="col-xs-12">
       <footer>
         <p>All Servers are managed here</p>
+        <hr>
+        <p>Made with ❤️ and 💻 by Rémi Mercier in {{ new Date().getFullYear() }}</p>
       </footer>
     </div>
   </div>
