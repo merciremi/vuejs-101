4b0c177574e2b6d48a52d49a47472220971ad73e
Remi Mercier <remi.merci@gmail.com>
2018-12-11 22:29:02 +0100

    Style components notice and footer

--- 014-components-communication/src/App.vue
+++ 014-components-communication/src/App.vue
@@ -4,10 +4,11 @@
   </div>
 </template>
 
-<style lang="scss">
+<style>
 body {
   background-color: rgba(238, 238, 238, 0.1);
 }
+
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -15,6 +16,7 @@ body {
   text-align: center;
   color: #2c3e50;
 }
+
 #nav {
   padding: 30px;
   a {
@@ -24,5 +26,15 @@ body {
       color: #42b983;
     }
   }
+}
+
+.component-notice {
+  font-size: 0.8em;
+  color: rgba(102, 204, 153, 1);
+  text-transform: uppercase;
+}
+
+.capitalize {
+  text-transform: capitalize;
 }
 </style>
--- 014-components-communication/src/components/shared/Footer.vue
+++ 014-components-communication/src/components/shared/Footer.vue
@@ -1,5 +1,13 @@
 <template>
- <p>Made with ❤️ and 💻 by Rémi Mercier in {{ new Date().getFullYear() }}</p>
+  <p>Made with ❤️ and 💻 by Rémi Mercier in <span class="date">{{ new Date().getFullYear() }}</span></p>
 </template>
 
-<script></script>
+<style scoped>
+p {
+  margin-top: 30px;
+}
+
+.date {
+  border-bottom: 3px solid rgba(106,216,106,0.5);
+}
+</style>
