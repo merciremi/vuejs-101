637bbf23241066ba58130fa5cdc8b2f6cd42fcae
Remi Mercier <remi.merci@gmail.com>
2018-12-14 10:47:39 +0100

    Create basic quotes list component

---
+++ 016-quotes-app/src/components/quote/QuotesList.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="quotes-list">
+    <p>{{ quotes.all }}</p>
+    <ul>
+      <li v-for="quote in quotes.all">{{ quote.content }}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['quotes']
+};
+</script>
+
+<style scoped>
+.quotes-list {
+  width: 100%;
+  border: 1px solid gray;
+  padding: 30px;
+  margin: 10px 0;
+  text-align: left;
+}
+</style>
