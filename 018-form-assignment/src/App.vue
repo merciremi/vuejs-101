954a40e968264754d0fe9ee6eb74df148b5f3b48
Remi Mercier <remi.merci@gmail.com>
2018-12-17 15:01:58 +0100

    Add footer to app

--- 018-form-assignment/src/App.vue
+++ 018-form-assignment/src/App.vue
@@ -45,6 +45,11 @@
         </div>
       </div>
     </div>
+    <div class="row">
+      <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+        <p>Made with ❤️ and 💻 by Rémi Mercier in <span class="date">{{ new Date().getFullYear() }}</span></p>
+    </div>
+    </div>
   </div>
 </template>
 
