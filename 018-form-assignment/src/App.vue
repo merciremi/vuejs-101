46447c631362f49a457d7214874549f073ede1da
Remi Mercier <remi.merci@gmail.com>
2018-12-17 14:33:15 +0100

    Replace name fields by component

--- 018-form-assignment/src/App.vue
+++ 018-form-assignment/src/App.vue
@@ -4,12 +4,7 @@
       <div class="row">
         <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
           <!-- Exercise 1 -->
-            <div class="form-group">
-              <label for="first-name">First Name</label>
-              <input type="text" id="first-name" class="form-control" v-model.lazy="user.firstName">
-              <label for="last-name">Last Name</label>
-              <input type="text" id="last-name" class="form-control" v-model.lazy="user.lastName">
-            </div>
+            <appFullName :user="user"></appFullName>
             <div class="form-group">
               <label for="email">Email</label>
               <input type="email" id="email" class="form-control" v-model="user.email">
@@ -28,10 +23,6 @@
             <div class="form-group">
               <input type="submit" value="Submit!" @click.prevent="submitted = !submitted">
             </div>
-          <!-- Exercise 2 -->
-          <!-- Only display the Form if it has NOT been submitted -->
-          <!-- Display the Data Summary ONCE the Form HAS been submitted -->
-
           <!-- Exercise 3 -->
           <!-- Edit the Example from above and create a custom "Full Name" Control -->
           <!-- which still holds the First Name and Last Name Input Field -->
@@ -58,6 +49,8 @@
 </template>
 
 <script>
+import FullName from '@/components/FullName.vue'
+
 export default {
   data() {
     return {
@@ -75,6 +68,9 @@ export default {
     fullName() {
       return this.user.firstName + ' ' + this.user.lastName;
     }
+  },
+  components: {
+    appFullName: FullName
   }
 };
 </script>
---
+++ 018-form-assignment/src/components/FullName.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="form-group">
+    <label for="first-name">First Name</label>
+    <input type="text" id="first-name" class="form-control" v-model.lazy="user.firstName">
+    <label for="last-name">Last Name</label>
+    <input type="text" id="last-name" class="form-control" v-model.lazy="user.lastName">
+  </div>
+</template>
+
+<script>
+export default {
+props: ['user']
+};
+</script>
+
+<style></style>
