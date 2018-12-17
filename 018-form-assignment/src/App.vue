e8f4dfa51847099a01f3619903cdd7a6d3a211ef
Remi Mercier <remi.merci@gmail.com>
2018-12-17 14:24:26 +0100

    Display form or recap depending on submission

--- 018-form-assignment/src/App.vue
+++ 018-form-assignment/src/App.vue
@@ -1,10 +1,9 @@
 <template>
   <div class="container">
-    <form>
+    <form v-if="submitted == false">
       <div class="row">
         <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
           <!-- Exercise 1 -->
-          <form action="">
             <div class="form-group">
               <label for="first-name">First Name</label>
               <input type="text" id="first-name" class="form-control" v-model.lazy="user.firstName">
@@ -29,13 +28,6 @@
             <div class="form-group">
               <input type="submit" value="Submit!" @click.prevent="submitted = !submitted">
             </div>
-          </form>
-          <!-- Create a Signup Form where you retrieve the following Information -->
-          <!-- Full Name (First Name + Last Name) -->
-          <!-- Mail -->
-          <!-- Password -->
-          <!-- Store Data? Yes/No -->
-
           <!-- Exercise 2 -->
           <!-- Only display the Form if it has NOT been submitted -->
           <!-- Display the Data Summary ONCE the Form HAS been submitted -->
@@ -45,9 +37,9 @@
           <!-- which still holds the First Name and Last Name Input Field -->
         </div>
       </div>
-    </form>
     <hr>
-    <div class="row">
+    </form>
+    <div class="row" v-if="submitted == true">
       <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
         <div class="panel panel-default">
           <div class="panel-heading">
