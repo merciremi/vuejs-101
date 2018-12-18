bda448ecfc4168dff9348c7b669e2714ab4c3a2e
Remi Mercier <remi.merci@gmail.com>
2018-12-18 17:04:07 +0100

    Create local and global filters

--- 020-filters-mixins/src/App.vue
+++ 020-filters-mixins/src/App.vue
@@ -1,19 +1,74 @@
 <template>
-    <div class="container">
-        <div class="row">
-            <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
-                <h1>Filters & Mixins</h1>
+  <div class="container">
+    <div class="row">
+      <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+        <h1 class="title">Filters & Mixins</h1>
+        <hr>
+        <p>{{ text | toUppercase | to-lowercase }}</p>
 
-            </div>
-        </div>
+      </div>
     </div>
+  </div>
 </template>
 
 <script>
-    export default {
+export default {
+  data() {
+    return {
+      text: 'Hello there'
     }
+  },
+  filters: {
+    toUppercase(value) {
+      return value.toUpperCase();
+    }
+  }
+};
 </script>
 
 <style>
+body {
+  padding: 50px;
+  text-align: center;
+  font-family: monospace;
+}
 
+h2 {
+  font-size: 2em;
+}
+
+p {
+  font-size: 16px;
+}
+
+hr {
+  margin: 40px 0;
+  border: 3px solid rgba(106,216,106,0.2);
+}
+
+footer {
+  margin-top: 40px;
+  padding-top: 20px;
+  border-top: 3px solid rgba(106,216,106,0.2);
+}
+
+.title {
+  margin: 0 auto;
+  padding-bottom: 20px;
+}
+
+.title h1 {
+  display: inline-block;
+  font-size: 3em;
+  font-weight: bold;
+  margin: 20px;
+  border-bottom: 3px solid rgba(106,216,106,0.5);
+  box-shadow: inset 0 -8px 0 rgba(106,216,106,0.5);
+}
+
+.output {
+  text-align: left;
+  padding-left: 20px;
+  border-left: 3px solid rgba(106,216,106,0.5);
+}
 </style>
--- 020-filters-mixins/src/main.js
+++ 020-filters-mixins/src/main.js
@@ -3,6 +3,10 @@ import App from './App.vue';
 
 Vue.config.productionTip = false;
 
+Vue.filter('to-lowercase', function(value) {
+  return value.toLowerCase();
+});
+
 new Vue({
   render: h => h(App),
 }).$mount('#app');
