8dc72280520405fe7e5f615d72907fdf2c729272
Remi Mercier <remi.merci@gmail.com>
2018-12-18 11:35:14 +0100

    Create custom directives with/without values and arguments

--- 019-built-in-directives/src/App.vue
+++ 019-built-in-directives/src/App.vue
@@ -8,11 +8,34 @@
     </div>
 
     <div class="row">
-      <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+      <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-8 col-md-offset-2">
         <h2>Built-in directives</h2>
         <p v-text="'Some text in a v-if directive'"></p>
         <p v-html="'<strong>Some html in a v-html directive (that should be sanitized).</strong>'"></p>
         <hr>
+      </div>
+    </div>
+
+    <div class="row">
+      <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-8 col-md-offset-2">
+        <h2>Custom directives</h2>
+        <div class="focus-block">
+          <p v-highlight>Paragraph colored with a custom directive</p>
+          <p v-highlightRed="'rgba(106,216,106,1)'">Paragraph colored with a custom directive <strong>passing a value</strong></p>
+          <p v-highlightArgument:background="'rgba(255, 255, 126, 1)'">Paragraph with a custom directive <strong>passing a :background argument</strong> to determine which - between background or font - should be colored</p>
+          <p v-highlightArgument="'rgba(255, 255, 126, 1)'">Same paragraph <strong>without the :background argument</strong></p>
+        </div>
+        <div class="output">
+          <strong>The five directives' hooks:</strong>
+          <ul>
+            <li>bind(el, binding, vnode) => Once the directive is attached to the element</li>
+            <li>inserted(el, binding, vnode) => Directive inserted in parent node</li>
+            <li>update(el, binding, vnode, oldVnode) => Once component is updated (without children)</li>
+            <li>componentUpdated(el, binding, vnode, oldVnode) => Once component is updated (with children)</li>
+            <li>unbind(el, binding, vnode) => Once the directive is removed from the element.</li>
+          </ul>
+          <code>binding, vnode, oldVnode should be treated as read only.</code>
+        </div>
       </div>
     </div>
   </div>
@@ -22,6 +45,11 @@
 
 <script>
 export default {
+  data() {
+    return {
+      userChoice: ''
+    }
+  }
 };
 </script>
 
@@ -69,5 +97,8 @@ footer {
   border-left: 3px solid rgba(106,216,106,0.5);
 }
 
+.focus-block {
+  margin: 50px 0;
+}
 
 </style>
--- 019-built-in-directives/src/main.js
+++ 019-built-in-directives/src/main.js
@@ -3,6 +3,28 @@ import App from './App.vue';
 
 Vue.config.productionTip = false;
 
+Vue.directive('highlight', {
+  bind(el, binding, vnode) {
+    el.style.backgroundColor = 'rgba(236, 100, 75, 1)';
+  }
+});
+
+Vue.directive('highlightRed', {
+  bind(el, binding, vnode) {
+    el.style.backgroundColor = binding.value;
+  }
+});
+
+Vue.directive('highlightArgument', {
+  bind(el, binding, vnode) {
+    if (binding.arg == 'background') {
+      el.style.backgroundColor = binding.value;
+    } else {
+      el.style.color = binding.value;
+    }
+  }
+});
+
 new Vue({
   render: h => h(App),
 }).$mount('#app');
